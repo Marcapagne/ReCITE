3e417dc59be4efcd8d0adef6524e9bcffc8f2af9
Allain John Notarte <itsallain@gmail.com>
2022-01-21 00:35:48 +0800

    Update CRUD.js

--- ReCITE/Web_Apps/CRUD.js
+++ ReCITE/Web_Apps/CRUD.js
@@ -1,88 +1,92 @@
-
-
 var classId = '';
 let gameType = '';
+
 function game_type(type) {
-    gameType = type;
-    getClassId().then(() => {
-        getData();
-        setTimeout(() => {
-            writeDailyRecord();
-        }, 1000);
-    })
+  gameType = type;
+  getClassId().then(() => {
+    getData();
+    setTimeout(() => {
+      writeDailyRecord();
+    }, 1000);
+  })
 }
 
 
 async function getClassId() {
-    await database.ref('key/classid').once('value').then(function (snapshot) {
-        classId = snapshot.val();
-    })
+  await database.ref('key/classid').once('value').then(function (snapshot) {
+    classId = snapshot.val();
+  })
 }
 
 function getData() {
-    var ref = database.ref('classList/' + classId + '/');
-    ref.on('value', function (snapshot) {
-        snapshot.forEach(element => {
-            samp_names.push(element.val().name);
-        });
+  var ref = database.ref('classList/' + classId + '/');
+  ref.on('value', function (snapshot) {
+    snapshot.forEach(element => {
+      samp_names.push(element.val().name);
     });
+  });
 }
 
 
 function updateOverall(name) {
-    database.ref('daily/' + classId +'/'+ gameType +'/'+ name + '/')
-        .once('value')
-        .then(function (snapshot) {
-            snapshot.ref.child('score').set(snapshot.val().score + 1);
-            getDailyScore(name);
-        });
+  database.ref('daily/' + classId + '/' + gameType + '/' + name + '/')
+    .once('value')
+    .then(function (snapshot) {
+      snapshot.ref.child('score').set(snapshot.val().score + 1);
+      getDailyScore(name);
+    });
 
 }
 
 async function getDailyScore(name) {
-    console.log(name)
-    database.ref('classList/' + classId + '/' + name + '/')
-        .once('value')
-        .then(function (snapshot) {
-            snapshot.ref.child('score').set(snapshot.val().score + 1);
-        });
+  console.log(name)
+  database.ref('classList/' + classId + '/' + name + '/')
+    .once('value')
+    .then(function (snapshot) {
+      snapshot.ref.child('score').set(snapshot.val().score + 1);
+    });
 }
 
 function writeDailyRecord() {
-      samp_names.forEach(name => {
-          var ref = database.ref('daily/'+ classId +'/'+ gameType +'/'+ name);
-          ref.set({
-              name: name,
-              score: 0,
-              lifeline: {
-                  cf: false,
-                  s: false,
-                  hu: false
-              }
-          })
-      })
-  }
+  samp_names.forEach(name => {
+    var ref = database.ref('daily/' + classId + '/' + gameType + '/' + name);
+    ref.set({
+      name: name,
+      score: 0,
+      lifeline: {
+        cf: false,
+        s: false,
+        hu: false
+      }
+    })
+  })
+}
 
 let selectedName = 'Kris';
 
 function addPoints() {
-    database.ref('daily/' + classId +'/'+ gameType +'/'+ selectedName + '/')
-        .once('value')
-        .then(function (snapshot) {
-            updateOverall(selectedName);
-        });
+  database.ref('daily/' + classId + '/' + gameType + '/' + selectedName + '/')
+    .once('value')
+    .then(function (snapshot) {
+      updateOverall(selectedName);
+    });
 }
 
 function getLifeLineStatus(name) {
-  database.ref('daily/'+ classId +'/'+ gameType +'/'+ selectedName + '/lifeline')
-  .once('value')
-  .then(function(snapshot) {
-      if(snapshot.val().cf) document.getElementById("cll").style.display = "none";
-      if(snapshot.val().s) document.getElementById("skp").style.display = "none";
-      if(snapshot.val().hu) document.getElementById("hdle").style.display = "none"; 
+  database.ref('daily/' + classId + '/' + gameType + '/' + selectedName + '/lifeline')
+    .once('value')
+    .then(function (snapshot) {
+      if (snapshot.val().cf) document.getElementById("cll").style.display = "none";
+      if (snapshot.val().s) document.getElementById("skp").style.display = "none";
+      if (snapshot.val().hu) document.getElementById("hdle").style.display = "none";
       callAFriend = snapshot.val().cf;
       skip = snapshot.val().s;
       huddleUp = snapshot.val().hu;
-      console.log(callAFriend)
-  });
+    });
+}
+
+function useLifeLine(specialAttack) {
+  database.ref('daily/' + classId + '/' + gameType + '/' + selectedName + '/lifeline/'+specialAttack)
+    .set(true);
+  getLifeLineStatus();
 }
